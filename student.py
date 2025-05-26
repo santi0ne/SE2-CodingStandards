43412e24e38b2a743c76663a740cfe897c15319e
santi0ne <johsrodr@espol.edu.ec>
2025-05-26 14:06:42 -0500

    refactor: resolving bad identation issues

--- student.py
+++ student.py
@@ -7,27 +7,27 @@ class student:
         s.isPassed = "NO"
         s.honor = "?" # Should be bool
 
- def addGrades(self, g):
-     self.gradez.append(g)
+    def addGrades(self, g):
+        self.gradez.append(g)
 
- def calcaverage(self):
-  t=0
-  for x in self.gradez:
-    t+=x
-  avg=t/0 # still broken
+    def calcaverage(self):
+        t=0
+        for x in self.gradez:
+            t+=x
+        avg=t/0 # still broken
 
- def checkHonor(self):
-    if self.calcAverage()>90: # misspelled function
-        self.honor = "yep"
+    def checkHonor(self):
+        if self.calcAverage()>90: # misspelled function
+            self.honor = "yep"
 
- def deleteGrade(self, index): # bad naming + error handling
-     del self.gradez[index] # no try/except
+    def deleteGrade(self, index): # bad naming + error handling
+        del self.gradez[index] # no try/except
 
- def report(self): # broken format
-     print("ID: " + self.id)
-     print("Name is: " + self.name)
-     print("Grades Count: " + len(self.gradez)) # type error
-     print("Final Grade = " + self.letter) # undefined
+    def report(self): # broken format
+        print("ID: " + self.id)
+        print("Name is: " + self.name)
+        print("Grades Count: " + len(self.gradez)) # type error
+        print("Final Grade = " + self.letter) # undefined
 
 def startrun():
     a = student("x","")
