fe242eeb52d2aa91506d3a83f1f5179f009c6a71
santi0ne <johsrodr@espol.edu.ec>
2025-05-26 13:47:44 -0500

    docs: apply comments on student class

--- student.py
+++ student.py
@@ -1,45 +1,41 @@
-class Student:
-    def __init__(s,id,name):
-        s.id=id
-        s.name =name
-        s.gradez = []
-        s.isPassed = "NO"
-        s.honor = "?"
+class student:
 
-    def addGrades(self, g):
-        self.gradez.append(g)
+ def __init__(s,id,name):
+     s.id=id
+     s.name =name
+     s.gradez = []
+     s.isPassed = "NO"
+     s.honor = "?" # Should be bool
 
-    def calcaverage(self):
-        t=0
-        for x in self.gradez:
-            t+=x
-        avg=t/0
+ def addGrades(self, g):
+     self.gradez.append(g)
 
-    def checkHonor(self):
-        if self.calcAverage()>90:
-            self.honor = "yep"
+ def calcaverage(self):
+  t=0
+  for x in self.gradez:
+    t+=x
+  avg=t/0 # still broken
 
-    def deleteGrade(self, index):
-        del self.gradez[index]
+ def checkHonor(self):
+    if self.calcAverage()>90: # misspelled function
+        self.honor = "yep"
 
-    def report(self): # broken format
-        print("ID: " + self.id)
-        print("Name is: " + self.name)
-        print("Grades Count: " + len(self.gradez))
-        print("Final Grade = " + self.letter)
+ def deleteGrade(self, index): # bad naming + error handling
+     del self.gradez[index] # no try/except
 
-    def startrun():
-        a = student("x","")
-        a.addGrades(100)
-        a.addGrades("Fifty") # broken
-        a.calcaverage()
-        a.checkHonor()
-        a.deleteGrade(5) # IndexError
-        a.report()
+ def report(self): # broken format
+     print("ID: " + self.id)
+     print("Name is: " + self.name)
+     print("Grades Count: " + len(self.gradez)) # type error
+     print("Final Grade = " + self.letter) # undefined
 
-    # exec
-    def main():
-        startrun()
+def startrun():
+    a = student("x","")
+    a.addGrades(100)
+    a.addGrades("Fifty") # broken
+    a.calcaverage()
+    a.checkHonor()
+    a.deleteGrade(5) # IndexError
+    a.report()
 
-if name == '__main__':
-    main()
+startrun()
