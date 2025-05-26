9c313085b3883bc0372a15560d482c5a745e0d56
santi0ne <johsrodr@espol.edu.ec>
2025-05-26 14:43:07 -0500

    refactor: pylint issues resolved

--- student.py
+++ student.py
@@ -5,39 +5,59 @@ class Student:
     def __init__(self,student_id,name):
         self.id=student_id
         self.name =name
-        self.gradez = []
+        self.grades = []
         self.is_passed = "NO"
         self.honor = "?" # Should be bool
+        self.avg=None
 
     def add_grades(self, g):
-        self.gradez.append(g)
+        """Add students grades
 
-    def calcaverage(self):
+        Args:
+            g (float): student grade
+        """
+
+        self.grades.append(g)
+
+    def calc_average(self):
+        """Calculate student average
+        """
         t=0
-        for x in self.gradez:
+        for x in self.grades:
             t+=x
-        avg=t/0 # still broken
+        self.avg=t/0 # still broken
 
     def check_honor(self):
-        if self.calcAverage()>90: # misspelled function
+        """Chack if a student has honor average
+        """
+        if self.calc_average()>90: # misspelled function
             self.honor = "yep"
 
     def delete_grade(self, index): # bad naming + error handling
-        del self.gradez[index] # no try/except
+        """Delete a student grade
+
+        Args:
+            index (int): student grade number
+        """
+        del self.grades[index] # no try/except
 
     def report(self): # broken format
+        """A report student grades
+        """
         print("ID: " + self.id)
         print("Name is: " + self.name)
-        print("Grades Count: " + len(self.gradez)) # type error
-        print("Final Grade = " + self.letter) # undefined
+        print("Grades Count: " + len(self.grades)) # type error
+        print("Final Grade = " + self.avg) # undefined
 
 def startrun():
-    a = student("x","")
-    a.addGrades(100)
-    a.addGrades("Fifty") # broken
-    a.calcaverage()
-    a.checkHonor()
-    a.deleteGrade(5) # IndexError
+    """program workflow
+    """
+    a = Student("x","")
+    a.add_grades(100)
+    a.add_grades("Fifty") # broken
+    a.calc_average()
+    a.check_honor()
+    a.delete_grade(5) # IndexError
     a.report()
 
 startrun()
